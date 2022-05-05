8c32621b38dcb18583248a6f98e8062d29b8505c
paulleerentveld <paul@veldnet.com.au>
2022-05-05 19:13:53 +1000

    Lab stuff

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React, { Component } from "react";
+
+export default class EyesOnMe extends Component {
+
+
+
+
+  render() {
+    return (
+      <div>
+          <button 
+          onFocus= {(e) => {
+              console.log("Good!")
+          }} 
+          onBlur= {(e) =>{
+              console.log("Hey! Eyes on me!")
+          } }
+          >Enter</button>
+      </div>
+    );
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+import React, { Component } from "react";
+
+export default class Keypad extends Component {
+
+
+
+  render() {
+    return (
+      <div>
+          <input type="password" onKeyUp= {(e) => {
+              console.log("Entering password...")
+          }} 
+          ></input>                
+        
+      </div>
+    );
+  }
+}
